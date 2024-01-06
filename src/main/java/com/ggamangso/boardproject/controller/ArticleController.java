5b60f543fb78f35ef455dc9623a2a49e057f71b7
ggamangso1 <ggamangso@gmail.com>
2024-01-06 11:02:48 +0900

    #16 - 게시글 관련 view 테스트 작성
    어떤 경로로 view를 열어줄지 테스트로 명세함

---
+++ src/main/java/com/ggamangso/boardproject/controller/ArticleController.java
@@ -0,0 +1,13 @@
+package com.ggamangso.boardproject.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/*
+/articles
+/articles/(article-id)
+/articles/search
+/articles/search-hashtag
+*/
+@RequestMapping("/articles")
+public class ArticleController {
+}
